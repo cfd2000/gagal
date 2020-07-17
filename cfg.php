c313d544c01f811a0ae5d470aafc3aa136abb91a
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2020-07-17 15:04:19 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+#MASUKAN EMAIL PASSWORD & DATA MILIK KALIAN!!!
+$email = "xxxxxxx";
+$password = "xxxxxx";
+$Authorization = "xxxxx";
+$deviceid = "xxxxxx";
+$clientid = "xxxxx";
+$eagleeye_sessionid = "xxxxxx";
+$eagleeye_pappname = "xxxxxx";
+$eagleeye_traceid = "xxxxx";
+$fingerprint = "xxxxx";
